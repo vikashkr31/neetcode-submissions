f7c22b13773839f8611d2f47db955e9804f12778
vikashkr31 <125289813+vikashkr31@users.noreply.github.com>
2026-06-09 21:30:14 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num = nums[0];
+        for(int i=1; i<nums.size();i++)
+            num = num ^ nums[i];
+
+        return num;
+    }
+};
